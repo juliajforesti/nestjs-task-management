0b70fdf44a4fc0184eb7814ce276efa2921c0db9
Julia Foresti <juliajforesti@gmail.com>
2021-03-17 14:33:22 -0300

    delete task implemented

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id)
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const {title, description} = createTaskDto
     const task: Task = {
@@ -23,4 +27,9 @@ export class TasksService {
     this.tasks.push(task)
     return task;
   }
+
+  deleteTask(id: string): void{
+    this.tasks = this.tasks.filter(task => task.id !== id)
+  }
+
 }
